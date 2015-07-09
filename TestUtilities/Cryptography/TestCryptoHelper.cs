8e75bcfeb5d17297a86c2c3fa006152243f2e8e5
Darren Maginnis <overmind.900@gmail.com>
2015-07-08 23:20:20 -0400

    Added CryptoHelper and tests

---
+++ TestUtilities/Cryptography/TestCryptoHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using NUnit.Framework;
+using Utilities.Cryptography;
+
+namespace TestUtilities.Cryptography {
+	[TestFixture]
+	public class TestCryptoHelper {
+
+
+		[Test]
+		public void EncryptDecrypt() {
+
+			using ( AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider() ) {
+				ICryptoTransform encryptor = aesAlg.CreateEncryptor();
+				ICryptoTransform decryptor = aesAlg.CreateDecryptor();
+				const string orignal = "Hello World";
+
+				byte[] encrypted = CryptoHelper.Encrypt( orignal, encryptor );
+				Assert.NotNull( encrypted );
+				Assert.IsNotEmpty( encrypted );
+
+				string decrypted = CryptoHelper.Decrypt( encrypted, decryptor );
+
+				Assert.AreEqual( orignal, decrypted );
+			}
+		}
+	}
+}
---
+++ Utilities/Cryptography/CryptoHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace Utilities.Cryptography {
+	public static class CryptoHelper {
+
+
+		public static string Decrypt( byte[] data, ICryptoTransform decryptor ) {
+			using ( MemoryStream msDecrypt = new MemoryStream( data ) ) {
+				using ( CryptoStream csDecrypt = new CryptoStream( msDecrypt, decryptor, CryptoStreamMode.Read ) ) {
+					using ( StreamReader srDecrypt = new StreamReader( csDecrypt ) ) {
+						// Read the decrypted bytes from the decrypting stream 
+						// and place them in a string.
+						return srDecrypt.ReadToEnd();
+					}
+				}
+			}
+		}
+
+		public static byte[] Encrypt( string data, ICryptoTransform encryptor ) {
+			using ( MemoryStream msEncrypt = new MemoryStream() ) {
+				using ( CryptoStream csEncrypt = new CryptoStream( msEncrypt, encryptor, CryptoStreamMode.Write ) ) {
+					using ( StreamWriter swEncrypt = new StreamWriter( csEncrypt ) ) {
+						//Write all data to the stream.
+						swEncrypt.Write( data );
+					}
+					return msEncrypt.ToArray();
+				}
+			}
+		}
+	}
+}
