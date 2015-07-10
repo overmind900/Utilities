6113f15ecddc2bc8511e8a68ce16fa9cb7615461
Darren Maginnis <overmind.900@gmail.com>
2015-07-10 14:48:52 -0400

    Add additional test cases

--- TestUtilities/Serialization/TestBinarySerializer.cs
+++ TestUtilities/Serialization/TestBinarySerializer.cs
@@ -4,6 +4,7 @@ using Utilities.Serialization;
 namespace TestUtilities.Serialization {
 	[TestFixture]
 	public class TestBinarySerializer {
+		
 		[Serializable]
 		public class TestBinarySerializerClass {
 			private int m_Int;
@@ -41,6 +42,118 @@ namespace TestUtilities.Serialization {
 			Assert.AreNotSame( orignal, deserialized );
 			Assert.AreEqual( orignal, deserialized );
 
+		}
+
+		[Serializable]
+		public class TestBinarySerializerClassB : TestBinarySerializerClass, IConvertible {
+			private double m_Double;
+
+			public double MyDouble {
+				get { return m_Double; }
+				set { m_Double = value; }
+			}
+
+			public override int GetHashCode() {
+				return base.GetHashCode();
+			}
+
+			public override bool Equals( object obj ) {
+				return base.Equals( obj ) && 
+					obj is TestBinarySerializerClassB &&
+					( (TestBinarySerializerClassB)obj ).m_Double.Equals(this.m_Double);
+			}
+			
+			#region IConvertible
+			public TypeCode GetTypeCode() {
+				throw new NotSupportedException();
+			}
+
+			public bool ToBoolean( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public byte ToByte( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public char ToChar( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public DateTime ToDateTime( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public decimal ToDecimal( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public double ToDouble( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public short ToInt16( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public int ToInt32( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public long ToInt64( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public sbyte ToSByte( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public float ToSingle( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public string ToString( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public object ToType( Type conversionType, IFormatProvider provider ) {
+				return this;
+			}
+
+			public ushort ToUInt16( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public uint ToUInt32( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			}
+
+			public ulong ToUInt64( IFormatProvider provider ) {
+				throw new NotSupportedException();
+			} 
+			#endregion
+
+		}
+
+		[Test]
+		public void SerializeDeserializeWithInharents() {
+			TestBinarySerializerClassB orignal = new TestBinarySerializerClassB() { MyInt = 4, MyString = "Hello World", MyDouble = 4.2 };
+			byte[] serialized = orignal.ToByteArray();
+			Assert.IsNotEmpty( serialized );
+			TestBinarySerializerClassB deserialized = serialized.FromByteArrayToObject<TestBinarySerializerClassB>();
+			Assert.AreNotSame( orignal, deserialized );
+			Assert.AreEqual( orignal, deserialized );
+		}
+
+		[Test]
+		public void SerializeDeserializeWithInharents2() {
+			TestBinarySerializerClass orignal = new TestBinarySerializerClassB() { MyInt = 4, MyString = "Hello World", MyDouble = 4.2 };
+			byte[] serialized = orignal.ToByteArray();
+			Assert.IsNotEmpty( serialized );
+			TestBinarySerializerClass deserialized = serialized.FromByteArrayToObject<TestBinarySerializerClass>();
+			Assert.AreNotSame( orignal, deserialized );
+			Assert.AreEqual( orignal, deserialized );
+			Assert.AreEqual( 4.2, ( (TestBinarySerializerClassB)deserialized ).MyDouble );
 		}
 	}
 }
