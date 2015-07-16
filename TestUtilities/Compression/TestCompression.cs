9b5774db51cb5d365022ccd5ad00a169fae1750b
Darren Maginnis <overmind.900@gmail.com>
2015-07-16 13:24:20 -0400

    Added Compression Extension

---
+++ TestUtilities/Compression/TestCompression.cs
@@ -0,0 +1,22 @@
+﻿using Utilities;
+using NUnit.Framework;
+using System.Text;
+
+namespace TestUtilities.Compression {
+	[TestFixture]
+	public partial class TestCompression {
+		 
+		[Test]
+		public void Compression() {
+			byte[] expected = Encoding.UTF8.GetBytes( m_XML );
+			byte[] compressed = expected.Compress();
+			Assert.IsNotEmpty( compressed );
+			Assert.Less( compressed.Length, expected.Length );
+
+			byte[] decompressed = compressed.Decompress();
+			Assert.Greater( decompressed.Length, compressed.Length );
+			Assert.AreEqual( expected, decompressed );
+			Assert.AreEqual( m_XML, Encoding.UTF8.GetString( decompressed ) );
+		}
+	}
+}
---
+++ Utilities/Compression/Extension.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+// ReSharper disable once CheckNamespace
+namespace Utilities {
+	public partial class Extension {
+
+		public static byte[] Compress( this byte[] data ) {
+			using ( MemoryStream outStream = new MemoryStream() ) {
+				using ( GZipStream zipStream = new GZipStream( outStream, CompressionMode.Compress ) ) {
+					using ( MemoryStream inStream = new MemoryStream( data ) ) {
+						inStream.CopyTo( zipStream );
+					}
+				}
+				return outStream.ToArray();
+			}
+		}
+
+		public static byte[] Decompress( this byte[] data ) {
+			using ( MemoryStream inStream = new MemoryStream( data ) ) {
+				using ( GZipStream zipStream = new GZipStream( inStream, CompressionMode.Decompress ) ) {
+					using ( MemoryStream outStream = new MemoryStream() ) {
+						zipStream.CopyTo( outStream );
+						return outStream.ToArray();
+					}
+				}
+			}
+		}
+	}
+}
